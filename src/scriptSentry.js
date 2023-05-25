6b1a0ac8ae7ec86f5c6c5643905d2b48303cc250
Marand <marpht23@gmail.com>
2023-05-25 18:30:20 +0200

    Adjust to new sentry layout

--- src/scriptSentry.js
+++ src/scriptSentry.js
@@ -1,34 +1,39 @@
 console.log("=== From scriptSentry.js")
 
-function addButtonIfTransactionInMessage() {
-    const messageText = document.querySelector("#message + div").textContent
+function addButtonIfTransactionInMessage(messageText) {
     const matchRes = messageText.match(/transact-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12}/g)
     if (matchRes) {
         console.log(matchRes[0])
 
-        const applicationInsightsSearchUrl = "REPLACE"
-        const html = `
-            <a class="linkBtn" href="${applicationInsightsSearchUrl}?searchTerm=${matchRes[0]}" target="_blank">
-                <span class="content">Follow Transaction</span>
+        const applicationInsightsSearchUrl = "https://portal.azure.com/#@AirmasterAS.onmicrosoft.com/resource/subscriptions/30a95655-48bf-45e0-9c40-d60a8b99d38c/resourceGroups/rg-airlinq-airmaster-prod-1/providers/Microsoft.Insights/components/appi-airlinq-airmaster-prod-1/searchV1"
+        const AIButton = `
+            <a class="linkBtn" href="${applicationInsightsSearchUrl}" target="_blank">
+                <span class="content">To Application Insights</span>
             </a>
         `;
         const placeholder = document.createElement("div");
-        placeholder.insertAdjacentHTML("afterbegin", html);
+        placeholder.insertAdjacentHTML("afterbegin", AIButton);
         const node = placeholder.firstElementChild;
 
-        const shareBtn = document.querySelector("button[aria-label='Share']")
+        const shareBtn = document.querySelector("button[aria-label='Copy Link']")
         shareBtn.after(node)
     }
 }
 
+let tryNum = 0, maxTries = 50
 function waitForMessageLoad() {
-    const messageEle = document.querySelector("#message")
+    const messageEle = document.querySelector(".exc-message")
+    if (tryNum > maxTries) {
+        console.log("Stopped trying")
+        return
+    }
     if (messageEle) {
-        addButtonIfTransactionInMessage()
+        addButtonIfTransactionInMessage(messageEle.textContent)
     } else {
         setTimeout(() => {
             waitForMessageLoad()
-        }, 100)
+            tryNum++
+        }, 500)
     }
 }
 
